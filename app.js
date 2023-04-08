7c5632582dbf52def1c0902c820dcb600d022b43
VIKAS KUMAR <kumar.vikas593@gmail.com>
2023-04-08 19:46:26 +0530

    express-app-5.0x

---
+++ app.js
@@ -0,0 +1,17 @@
+import  express  from 'express'
+const app = express()
+const port = process.env.PORT || '8000'
+
+
+app.get('/', (req, res) => {
+      res.send('hello world!')
+    })
+
+
+
+app.listen(port, () => {
+console.log(`server listening at http://localhost:${port}`)
+})
+
+
+
