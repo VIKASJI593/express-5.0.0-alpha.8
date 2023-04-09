224df4f502d1c59c2106cacbb15c3495e91d75a7
VIKAS KUMAR <kumar.vikas593@gmail.com>
2023-04-09 12:36:07 +0530

    router

--- app.js
+++ app.js
@@ -3,12 +3,15 @@
 // const tea =require ('./routes/teacher.js');
 
 import express from "express";
+
+//-------------4th step--------import routes of student------------
 import stu from "./routes/student.js";
 import tea from "./routes/teacher.js";
 
 const app = express();
 const port = process.env.PORT || "8000";
 
+//---------5th step-----------load router modules----------------
 app.use("/vidyarthi", stu);
 app.use("/sikshak", tea);
 
--- routes/student.js
+++ routes/student.js
@@ -1,7 +1,8 @@
+//------------1st step---------import express and create route object--------
 import express from "express"
 const router = express.Router()
 
-
+//----------2nd step---------router method------------
 router.post("/create", (req, res) => {
   res.send("ADD NEW STUDENT!");
 });
@@ -19,5 +20,6 @@ router.delete("/delete", (req, res) => {
     res.send("DELETE STUDENT!");
   });
 
+  //-------3rd step-------------export router---------------
 //module.exports = router
 export default router;
